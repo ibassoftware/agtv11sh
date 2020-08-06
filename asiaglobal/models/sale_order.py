ca93d0fa625946a3b1b5b73be6f8895170498c80
Lawrence <jovenlawrencegersaniba.fcu@gmail.com>
2020-08-06 17:28:01 +0800

    modified invoice_status

--- asiaglobal/models/sale_order.py
+++ asiaglobal/models/sale_order.py
@@ -1,47 +1,56 @@
-from odoo import models, fields, api, _ 
+from odoo import models, fields, api, _
 from odoo.addons import decimal_precision as dp
 
+
 class SaleOrder(models.Model):
-	_inherit = 'sale.order'
+    _inherit = 'sale.order'
 
-	# NEW FIELDS
-	jo_id = fields.Many2one('asiaglobal.job_order', string='Job Order')
-	sale_source = fields.Selection([
-		('exhibit', 'Exhibit'),
-		('new', 'New Client'),
-		('referral', 'Referral'),
-		('saturation', 'Saturation'),
-		('old', 'Old Customer'),
-		('other', 'Others'),
-	], string='Source of Sale')
+    # NEW FIELDS
+    jo_id = fields.Many2one('asiaglobal.job_order', string='Job Order')
+    sale_source = fields.Selection([
+        ('exhibit', 'Exhibit'),
+        ('new', 'New Client'),
+        ('referral', 'Referral'),
+        ('saturation', 'Saturation'),
+        ('old', 'Old Customer'),
+        ('other', 'Others'),
+    ], string='Source of Sale')
 
-	# OVERRIDE
-	validity_date = fields.Date(states={'draft': [('readonly', False)], 'manager_approval': [('readonly', False)], 'admin_approval': [('readonly', False)], 'approved': [('readonly', False)], 'sent': [('readonly', False)]},)
+    # OVERRIDE
+    validity_date = fields.Date(states={'draft': [('readonly', False)], 'manager_approval': [('readonly', False)], 'admin_approval': [
+                                ('readonly', False)], 'approved': [('readonly', False)], 'sent': [('readonly', False)]},)
+    invoice_status = fields.Selection([
+        ('upselling', 'Upselling Opportunity'),
+        ('invoiced', 'Fully Invoiced'),
+        ('to invoice', 'To Invoice'),
+        ('no', 'Invoiced')
+    ], string='Invoice Status', compute='_get_invoiced', store=True, readonly=True)
+
 
 class SaleOrderLine(models.Model):
-	_inherit = 'sale.order.line'
+    _inherit = 'sale.order.line'
 
-	price_unit = fields.Float(digits=dp.get_precision('Sale Product Price'))
+    price_unit = fields.Float(digits=dp.get_precision('Sale Product Price'))
 
-	@api.multi
-	@api.onchange('product_id')
-	def product_id_change(self):
-		result = super(SaleOrderLine, self).product_id_change()
-		vals = {}
-		product = self.product_id.with_context(
-			lang=self.order_id.partner_id.lang,
-			partner=self.order_id.partner_id.id,
-			quantity=vals.get('product_uom_qty') or self.product_uom_qty,
-			date=self.order_id.date_order,
-			pricelist=self.order_id.pricelist_id.id,
-			uom=self.product_uom.id
-		)
-		name = product.default_code or ''
-		if product.description_sale:
-			name += '\n' + product.description_sale
-		# If no description and item code
-		if not name:
-			name = product.name
-		vals['name'] = name
-		self.update(vals)
-		return result
+    @api.multi
+    @api.onchange('product_id')
+    def product_id_change(self):
+        result = super(SaleOrderLine, self).product_id_change()
+        vals = {}
+        product = self.product_id.with_context(
+            lang=self.order_id.partner_id.lang,
+            partner=self.order_id.partner_id.id,
+            quantity=vals.get('product_uom_qty') or self.product_uom_qty,
+            date=self.order_id.date_order,
+            pricelist=self.order_id.pricelist_id.id,
+            uom=self.product_uom.id
+        )
+        name = product.default_code or ''
+        if product.description_sale:
+            name += '\n' + product.description_sale
+        # If no description and item code
+        if not name:
+            name = product.name
+        vals['name'] = name
+        self.update(vals)
+        return result
