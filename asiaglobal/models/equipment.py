45ddaf030114b0ec37db7541c0773044c44f359d
Reynaflor Sentillas <reynaflorsentillas@ymail.com>
2020-08-09 14:46:09 +0800

    Revert "Merge AGT-03-08-2020 into master"
    This reverts commit 438d21d32f1c3f322d4918615cc04343093249fa, reversing
    changes made to c83e7064000e66ab4beebd9900a3f6209e374aa7.

--- asiaglobal/models/equipment.py
+++ asiaglobal/models/equipment.py
@@ -102,7 +102,6 @@ class AsiaGlobalEquipmentProfile(models.Model):
     manufacturer = fields.Many2one('asiaglobal.manufacturer')
     model = fields.Many2one('asiaglobal.manufacturer_model')
     serial_number = fields.Char()
-    profile_image = fields.Binary()
 
     date_in_service = fields.Date(required=True, default=fields.Datetime.now())
     type = fields.Many2one('asiaglobal.equipment_type')
@@ -185,11 +184,6 @@ class AsiaGlobalEquipmentProfile(models.Model):
 
     job_material_request_ids = fields.Many2many(
         'asiaglobal.job_material_request_form', string='Job Material Request Form', compute='_compute_job_material_request')
-
-    # HISTORY
-
-    history_ids = fields.One2many(
-        'asiaglobal.equipment_history', 'equipment_id', string='History')
 
     @api.multi
     @api.onchange('customer')
